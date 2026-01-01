f639cebc486de31b89df2d5cb6821d4ddc413d20
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-01 18:47:50 +0530

    quality of life improvement

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import "time"
+
 func main() {
 	config := DefaultConfig()
 
+	room := CreateRoom(&config)
+	go room.StartListening()
+	time.Sleep(10 * time.Second)
 }
--- room.go
+++ room.go
@@ -16,9 +16,26 @@ func CreateRoom(config *Config) Room {
 		log.Fatalf("failed to open tcp server: %s\n", err)
 	}
 
-	room := Room{
-		clients:  nil,
+	log.Printf("started listening on port %s\n", config.port)
+
+	return Room{
+		clients:  []bool{},
 		listener: listener,
 	}
+}
 
+func (room *Room) StartListening() {
+	defer func() {
+		err := room.listener.Close()
+		if err != nil {
+			log.Fatalf("failed to close server %s\n", err)
+		}
+	}()
+
+	for {
+		_, err := room.listener.Accept()
+		if err != nil {
+			log.Fatalf("failed to connect to %s\n", err)
+		}
+	}
 }
